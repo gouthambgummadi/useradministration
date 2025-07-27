3145a0bdac4b3832b7a5ffe689dc76b03f458468
gouthambgummadi <gouthamchowdary77147@yahoo.com>
2025-07-27 09:16:05 -0500

    Update userinfo.ps1

--- userinfo.ps1
+++ userinfo.ps1
@@ -1,8 +1,6 @@
-get-aduser goutham.gummadi
-# Example: Local AD
-# Get-ADUser -Identity "gou.gumm"
+
 
 # Example: Azure AD via Graph PowerShell
 Connect-MgGraph -Scopes "User.Read.All"
 
-Get-MgUser -UserId "gou.gumm@yourdomain.com"
+Get-MgUser -UserId "goutham.gummadi@edhc.com"
